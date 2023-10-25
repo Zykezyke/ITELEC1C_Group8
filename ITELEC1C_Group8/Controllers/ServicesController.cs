483c4c31c04fc1512a03db2f8043f0c4039c816a
zyke <zyke.victoria.cics@ust.edu.ph>
2023-10-26 07:47:19 +0800

    Add project files.

---
+++ ITELEC1C_Group8/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITELEC1C_Group8.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
