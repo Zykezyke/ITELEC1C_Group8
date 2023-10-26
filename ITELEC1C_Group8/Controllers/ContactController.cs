0a740a24985251b60ee79af8536e152fe1f915d9
kylacruise <kylamarielle.cruz.cics@ust.edu.ph>
2023-10-26 08:24:34 +0800

    contact controller

---
+++ ITELEC1C_Group8/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITELEC1C_Group8.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
