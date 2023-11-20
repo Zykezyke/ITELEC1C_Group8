5152fe469028876148cb52a3ed621cb855b62ef3
zyke <zyke.victoria.cics@ust.edu.ph>
2023-11-20 19:28:41 +0800

    now cant enter appointment or contact us unless logged in

--- ITELEC1C_Group8/Controllers/AppointmentController.cs
+++ ITELEC1C_Group8/Controllers/AppointmentController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITELEC1C_Group8.Controllers
 {
     public class AppointmentController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
             return View();
